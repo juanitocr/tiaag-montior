e8672a2919a618146cf4be9e192791e3e9d0fec1
juanmcel <juanmcel2697@gmail.com>
2019-01-30 14:39:45 -0600

    cambio 30 de enero

--- monitortiaag/src/app/app.module.ts
+++ monitortiaag/src/app/app.module.ts
@@ -8,15 +8,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCj54TG5JUzPp0FNpuAvEdQcun_iozInJU",
+  authDomain: "nodemcu-accc0.firebaseapp.com",
+  databaseURL: "https://nodemcu-accc0.firebaseio.com",
+  projectId: "nodemcu-accc0",
+  storageBucket: "nodemcu-accc0.appspot.com",
+  messagingSenderId: "1035967596855"
+}
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig,'nodemcu'),
+    AngularFireDatabaseModule
   ],
   providers: [
     StatusBar,
@@ -25,4 +36,6 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {}
+
---
+++ monitortiaag/src/app/services/datos.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatosService {
+
+
+  constructor(private angularFireDatabase: AngularFireDatabase) {
+  }
+
+    getInfoSheepMonitor(id){
+    return this.angularFireDatabase.object('/');
+    
+    
+
+   }
+}
